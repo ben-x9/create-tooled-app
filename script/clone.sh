5c84b1bdbd3a6804efe3e1e4aa8a64089be711d5
Ben Lambert <ben@joyxer.com>
2018-05-22 20:47:31 +0900

    Exclude clone script from clone operation

--- script/clone.sh
+++ script/clone.sh
@@ -5,7 +5,8 @@ rsync -av --progress . ../$1 \
   --exclude node_modules \
   --exclude platforms \
   --exclude plugins \
-  --exclude .git
+  --exclude .git \
+  --exclude script/clone.sh
 else
   echo "That directory already exists!"
 fi
