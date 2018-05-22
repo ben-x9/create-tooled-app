899c366deb11c8543939bae225afcb0284cb8fc3
Ben Lambert <ben@joyxer.com>
2018-05-22 20:48:00 +0900

    Formatting

--- script/clone.sh
+++ script/clone.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ ! -d "../$1" ]; then
-rsync -av --progress . ../$1 \
-  --exclude node_modules \
-  --exclude platforms \
-  --exclude plugins \
-  --exclude .git \
-  --exclude script/clone.sh
+  rsync -av --progress . ../$1 \
+    --exclude node_modules \
+    --exclude platforms \
+    --exclude plugins \
+    --exclude .git \
+    --exclude script/clone.sh
 else
   echo "That directory already exists!"
 fi
